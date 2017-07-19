3766658d6ddf0e2a83a013e784f41d2822e1f82e
Karol Będkowski <karol.bedkowski@gmail.com>
2017-07-19 22:17:53 +0200

    /info endpoint for local connections

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	_ "net/http/pprof"
 	"os"
 	"os/signal"
+	"strings"
 	"sync"
 	"sync/atomic"
 	"syscall"
@@ -294,6 +295,70 @@ func (q queryHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.With("req_id", requestID).Debugf("done")
 }
 
+type infoHndler struct {
+	Configuration *Configuration
+}
+
+func (q infoHndler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	if !strings.HasPrefix(r.RemoteAddr, "127.0.0.1:") && !strings.HasPrefix(r.RemoteAddr, "localhost:") {
+		http.Error(w, "forbidden", http.StatusForbidden)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+	w.Write([]byte("DATABASES\n========="))
+	for name, db := range q.Configuration.Database {
+		w.Write([]byte("\n"))
+		w.Write([]byte(name))
+		w.Write([]byte("\n-----"))
+		w.Write([]byte("\n - driver: "))
+		w.Write([]byte(db.Driver))
+		w.Write([]byte("\n - connection:\n"))
+		for k, v := range db.Connection {
+			w.Write([]byte("    - "))
+			w.Write([]byte(k))
+			w.Write([]byte(": "))
+			if strings.HasPrefix(strings.ToLower(k), "pass") {
+				w.Write([]byte("*"))
+			} else {
+				w.Write([]byte(fmt.Sprintf("%v", v)))
+			}
+			w.Write([]byte("\n"))
+		}
+		w.Write([]byte(" - labels:\n"))
+		for k, v := range db.Labels {
+			w.Write([]byte("    - "))
+			w.Write([]byte(k))
+			w.Write([]byte(": "))
+			w.Write([]byte(fmt.Sprintf("%v", v)))
+			w.Write([]byte("\n"))
+		}
+	}
+
+	w.Write([]byte("\n\nQueries\n======="))
+	for name, q := range q.Configuration.Query {
+		w.Write([]byte("\n"))
+		w.Write([]byte(name))
+		w.Write([]byte("\n-----"))
+		w.Write([]byte("\n - sql: "))
+		w.Write([]byte(q.SQL))
+		w.Write([]byte("\n - caching time: "))
+		w.Write([]byte(fmt.Sprintf("%v", q.CachingTime)))
+		w.Write([]byte("\n - metrics:\n"))
+		w.Write([]byte(q.Metrics))
+		w.Write([]byte("\n - params:\n"))
+		for k, v := range q.Params {
+			w.Write([]byte("    - "))
+			w.Write([]byte(k))
+			w.Write([]byte(": "))
+			w.Write([]byte(fmt.Sprintf("%v", v)))
+			w.Write([]byte("\n"))
+		}
+	}
+}
+
 func main() {
 	flag.Parse()
 
@@ -322,6 +387,7 @@ func main() {
 				if newConf, err := loadConfiguration(*configFile); err == nil {
 					handler.Configuration = newConf
 					handler.clearCache()
+					iHandler.Configuration = newConf
 					log.Info("configuration reloaded")
 				} else {
 					log.Errorf("reloading configuration err: %s", err)
@@ -333,6 +399,7 @@ func main() {
 
 	http.Handle("/metrics", promhttp.Handler())
 	http.Handle("/query", prometheus.InstrumentHandler("query", handler))
+	http.Handle("/info", prometheus.InstrumentHandler("info", iHandler))
 	log.Infof("Listening on %s", *listenAddress)
 	log.Fatal(http.ListenAndServe(*listenAddress, nil))
 }
